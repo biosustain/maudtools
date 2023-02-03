ee32c8008acdff64b92aa47c97c734099bf9464a
Teddy Groves <groves.teddy@gmail.com>
2023-02-03 11:55:05 +0100

    Update plotting

--- tests/test_unit/test_plotting_variables.py
+++ tests/test_unit/test_plotting_variables.py
@@ -1,41 +1,27 @@
-import os
-
 import arviz
 import pytest
-from maud.data_model.stan_variable_set import (
-    ConcEnzyme,
-    ConcUnbalanced,
-    Dgf,
-    Drain,
-    Kcat,
-    Ki,
-    Km,
-)
+from importlib_resources import files
+from maud.data.example_outputs import linear
 from maud.loading_maud_inputs import load_maud_input
 
 from maudtools.plotting import plot_maud_prior, plot_maud_variable
 
-DATA_PATH = os.path.join(
-    os.path.dirname(__file__), "..", "data", "input_G6PtoPEP"
-)
-OUTP_PATH = os.path.join(
-    os.path.dirname(__file__), "..", "data", "methionine_output", "idata.nc"
-)
-
 
 @pytest.mark.parametrize(
-    "stan_var", [ConcEnzyme, ConcUnbalanced, Dgf, Drain, Kcat, Ki, Km]
+    "stan_var",
+    ["conc_enzyme_train", "conc_unbalanced_train", "dgf", "kcat", "ki", "km"],
 )
 def test_plot_posterior_and_prior(stan_var):
-    mi = load_maud_input(DATA_PATH)
-    idata = arviz.from_netcdf(OUTP_PATH)
-    plot_maud_variable(idata, mi.priors, stan_var)
+    mi = load_maud_input(files(linear).joinpath("user_input"))
+    idata = arviz.from_json(files(linear).joinpath("idata.json"))
+    plot_maud_variable(idata, getattr(mi.parameters, stan_var))
 
 
 @pytest.mark.parametrize(
-    "stan_var", [ConcEnzyme, ConcUnbalanced, Dgf, Drain, Kcat, Ki, Km]
+    "stan_var",
+    ["conc_enzyme_train", "conc_unbalanced_train", "dgf", "kcat", "ki", "km"],
 )
 def test_plot_prior_alone(stan_var):
-    mi = load_maud_input(DATA_PATH)
-    idata = arviz.from_netcdf(OUTP_PATH)
-    plot_maud_prior(idata, mi.priors, stan_var, concat=False)
+    mi = load_maud_input(files(linear).joinpath("user_input"))
+    idata = arviz.from_json(files(linear).joinpath("idata.json"))
+    plot_maud_prior(idata, getattr(mi.parameters, stan_var), concat=False)
